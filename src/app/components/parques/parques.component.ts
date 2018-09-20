859f7b2a52b200e8d1c558219af7dc44d82a5637
daniloenriquediazrios <danilodiaz2044@gmail.com>
2018-09-19 21:11:44 -0500

    prueba

---
+++ src/app/components/parques/parques.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'parques',
+    templateUrl: './parques.component.html',
+})
+export class ParquesComponent {
+    @Input() nombre: string;
+    @Input('metros_cuadrados') metros: number;
+    public vegetacion: string;
+    public abierto: boolean;
+
+    constructor(){
+        this.nombre = 'Parque natural para caballos';
+        this.metros = 450;
+        this.vegetacion = 'Alta';
+        this.abierto = false;
+    }
+
+}
