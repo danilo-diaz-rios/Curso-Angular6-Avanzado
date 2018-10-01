fe27e24169fa4b5c286f8b237d486319da69d43f
Carlos <caabcarlos27@gmail.com>
2018-10-01 10:26:50 -0500

    creacion de modelos y el formulario de registro

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { User } from '../../models/user';
 
 @Component({
     selector: 'register',
@@ -7,12 +8,14 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 })
 export class RegisterComponent implements OnInit{
     public title: String;
+    public user: User;
 
     constructor(
         private _route: ActivatedRoute,
         private _router: Router
     ){
         this.title = 'Registro';
+        this.user = new User('','','','','','ROLE_USER');
     }
 
     ngOnInit(){
